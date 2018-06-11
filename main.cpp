47d89d1f42afdcc02f59c49f0beb79139e7bb197
linwei <zlinwei@zlinwei.com>
2018-06-11 04:52:16 -0400

    can not use std::cout at acc parallel loop

--- main.cpp
+++ main.cpp
@@ -2,13 +2,32 @@
 #include <openacc.h>
 #include <omp.h>
 
+#include <cstdio>
+
 int main() {
-    std::cout << acc_get_device_num(acc_device_host) << std::endl;
-    std::cout << omp_get_max_threads() << std::endl;
+    std::cout << "acc device num: " << acc_get_device_num(acc_device_host) << std::endl;
+    std::cout << "omp max threads: " << omp_get_max_threads() << std::endl;
 
 #pragma omp parallel
     {
-        std::cout << "Hello, World! " << omp_get_thread_num() << std::endl;
+        std::cout << omp_get_thread_num();
     };
+    std::cout << std::endl;
+
+    int a[10] = {0};
+
+#pragma acc parallel loop
+    for (int i = 0; i < 10; ++i) {
+        a[i] = i;
+        printf("%d", i);
+    }
+
+    std::cout << std::endl;
+#pragma omp parallel for
+    for (int j = 0; j < 10; ++j) {
+        std::cout << a[j];
+    }
+    std::cout << std::endl;
+
     return 0;
 }
